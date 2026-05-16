d78bc5eb256ada19d645def025e3cbf2d40295c2
Ashutosh Aman <69684043+AmanAshutosh@users.noreply.github.com>
2026-05-16 22:32:59 +0530

    programming 101

--- Warm Up/Programming 101.md
+++ Warm Up/Programming 101.md
@@ -2,6 +2,9 @@
 ---
 - For Beginners
 - Easy to Follow
+- Everything Practical
+- Level 0 for Namaste Dev
+
 
 
 
---
+++ Warm Up/Programming101.js
@@ -0,0 +1,4 @@
+console.log("hello world") // first 
+console.log("Ashutosh") // prints "string"
+console.log(7) // prints number
+console.log(true) // prints bollean
